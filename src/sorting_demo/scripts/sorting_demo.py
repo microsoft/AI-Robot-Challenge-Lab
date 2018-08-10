25fcfca7ad82ef983cbe5e2140bea5e6ff24b870
Manuel Caballero Sanchez <minolo3ds@gmail.com>
2018-08-10 15:13:03 +0200

    Improved code reuse on model spawning

--- src/sorting_demo/scripts/sorting_demo.py
+++ src/sorting_demo/scripts/sorting_demo.py
@@ -7,6 +7,7 @@ import copy
 
 import rospy
 import rospkg
+import xacro
 
 from gazebo_msgs.srv import (
     SpawnModel,
@@ -142,38 +143,68 @@ class SortingDemo(object):
         # retract to clear object
         self._retract()
 
-
-def load_gazebo_models(table_pose=Pose(position=Point(x=0.75, y=0.0, z=0.0)),
-                       table_reference_frame="world",
-                       block_pose=Pose(position=Point(x=0.4225, y=0.1265, z=0.7725)),
-                       block_reference_frame="world"):
-    # Get Models' Path
-    model_path = rospkg.RosPack().get_path('sawyer_sim_examples') + "/models/"
-    # Load Table SDF
-    table_xml = ''
-    with open(model_path + "cafe_table/model.sdf", "r") as table_file:
-        table_xml = table_file.read().replace('\n', '')
-    # Load Block URDF
-    block_xml = ''
-    with open(model_path + "block/model.urdf", "r") as block_file:
-        block_xml = block_file.read().replace('\n', '')
-    # Spawn Table SDF
-    rospy.wait_for_service('/gazebo/spawn_sdf_model')
-    try:
-        spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-        resp_sdf = spawn_sdf("cafe_table", table_xml, "/",
-                             table_pose, table_reference_frame)
-    except rospy.ServiceException, e:
-        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
-    # Spawn Block URDF
+def spawn_urdf(name, description_xml, pose, reference_frame):
     rospy.wait_for_service('/gazebo/spawn_urdf_model')
     try:
         spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
-        resp_urdf = spawn_urdf("block", block_xml, "/",
-                               block_pose, block_reference_frame)
+        resp_urdf = spawn_urdf(name, description_xml, "/", pose, reference_frame)
     except rospy.ServiceException, e:
         rospy.logerr("Spawn URDF service call failed: {0}".format(e))
 
+def load_xacro_file(file_path, mappings):
+    urdf_doc = xacro.process_file(file_path, mappings=mappings)
+    urdf_xml = urdf_doc.toprettyxml(indent='  ', encoding='utf-8')
+    urdf_xml = urdf_xml.replace('\n', '')
+    return urdf_xml
+
+def spawn_xacro_model(name, path, pose, reference_frame, mappings):
+    description_xml = load_xacro_file(path, mappings)
+    spawn_urdf(name, description_xml, pose, reference_frame)
+
+def spawn_urdf_model(name, path, pose, reference_frame):
+    description_xml = ''
+    with open(path, "r") as model_file:
+        description_xml = model_file.read().replace('\n', '')
+
+    spawn_urdf(name, description_xml, pose, reference_frame)
+
+def spawn_sdf_model(name, path, pose, reference_frame):
+    # Load Model SDF
+    description_xml = ''
+    with open(path, "r") as model_file:
+        description_xml = model_file.read().replace('\n', '')
+
+    # Spawn Model SDF
+    rospy.wait_for_service('/gazebo/spawn_sdf_model')
+    try:
+        spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+        resp_sdf = spawn_sdf(name, description_xml, "/", pose, reference_frame)
+    except rospy.ServiceException, e:
+        rospy.logerr("Spawn SDF service call failed: {0}".format(e))
+
+def load_gazebo_models():
+    # Spawn Table
+    table_name = "cafe_table"
+    table_path = rospkg.RosPack().get_path('sawyer_sim_examples') + "/models/cafe_table/model.sdf"
+    table_pose = Pose(position=Point(x=0.75, y=0.0, z=0.0))
+    table_reference_frame = "world"
+    spawn_sdf_model(table_name, table_path, table_pose, table_reference_frame)
+
+    # Spawn blocks
+    block_path = rospkg.RosPack().get_path('sorting_demo') + "/models/block/block.urdf.xacro"
+    block_reference_frame = "world"
+
+    block_poses = [
+        Pose(position=Point(x=0.4225, y=0.1265, z=0.7725)),
+        Pose(position=Point(x=0.60, y=0.1265, z=0.7725)),
+        Pose(position=Point(x=0.4225, y=-0.1, z=0.7725))]
+    block_mappings = [
+        {"material" : "Gazebo/Green"},
+        {"material" : "Gazebo/Orange"},
+        {"material" : "Gazebo/SkyBlue"}]
+
+    for (i, pose, mappings) in zip(range(len(block_poses)), block_poses, block_mappings):
+        spawn_xacro_model("block{}".format(i), block_path, pose, block_reference_frame, mappings)
 
 def delete_gazebo_models():
     # This will be called on ROS Exit, deleting Gazebo models
