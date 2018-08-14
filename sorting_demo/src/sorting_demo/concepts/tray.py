1a5705559d2066899683de6821001394708c9e41
Pablo Iñigo Blasco <pibgeus@gmail.com>
2018-08-14 10:24:46 +0200

    faster speed, gripper ismovingcheck

--- sorting_demo/src/sorting_demo/concepts/tray.py
+++ sorting_demo/src/sorting_demo/concepts/tray.py
@@ -33,7 +33,7 @@ class TrayState:
         #return self.final_pose
         copyfinalpose = copy.deepcopy(self.final_pose)
 
-        yoffset = -0.05 + 0.075 * len(self.blocks)
+        yoffset = -0.1 + 0.1 * len(self.blocks)
         copyfinalpose.position.y -= yoffset
         copyfinalpose.position.z += 0.03
 
