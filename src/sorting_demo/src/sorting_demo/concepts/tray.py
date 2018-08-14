b906bad36de825230b7faf1b02a14b437c70cd9d
Pablo Iñigo Blasco <pibgeus@gmail.com>
2018-08-14 10:36:01 +0200

    a bit more thicker tray

--- src/sorting_demo/src/sorting_demo/concepts/tray.py
+++ src/sorting_demo/src/sorting_demo/concepts/tray.py
@@ -25,7 +25,7 @@ class TrayState:
 
     def get_tray_pick_location(self):
         copyfinalpose = copy.deepcopy(self.final_pose)
-        copyfinalpose.position.y -= 0.19
+        copyfinalpose.position.y -= 0.0
         copyfinalpose.position.z -= 0.02
         return copyfinalpose
 
@@ -33,7 +33,7 @@ class TrayState:
         #return self.final_pose
         copyfinalpose = copy.deepcopy(self.final_pose)
 
-        yoffset = -0.1 + 0.075 * len(self.blocks)
+        yoffset = -0.08 + 0.075 * len(self.blocks)
         copyfinalpose.position.y -= yoffset
         copyfinalpose.position.z += 0.03
 
