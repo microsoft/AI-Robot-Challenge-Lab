0d4d89c950b430f4db0681a708abbf322d356d01
Paul Stubbs <pstubbs@microsoft.com>
2018-08-14 15:17:55 -0700

    work in progress

--- setup/ubuntu/bot-stats-node.py
+++ setup/ubuntu/bot-stats-node.py
@@ -2,20 +2,26 @@
 
 import rospy
 from std_msgs.msg import String
+from intera_core_msgs.msg import AssemblyState
+import sys
+import json
 
 print 'bot-stats-node starting'
 
 
 def callback(data):
     print 'demostats callback'
-    rospy.loginfo("I heard %s",data.data)
+    #rospy.loginfo("I heard %s",data.data)
+    print json.dumps(data)
     
-def listener():
+def main():
     print 'demostats listener'
     rospy.init_node('Demo_Stats')
-    rospy.Subscriber("/robot/state", String, callback)
+    rospy.Subscriber("/robot/state", AssemblyState, callback)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
-if __name__ == "__main__":
-    listener()
+
+if __name__ == '__main__':
+    sys.exit(main())
+
