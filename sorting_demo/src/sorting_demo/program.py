cedbd6457d14f149e048a42ca8a457e9c678adea
Pablo Iñigo Blasco <pibgeus@gmail.com>
2018-08-28 02:22:05 +0200

    adding more funcionality to task facade

--- sorting_demo/src/sorting_demo/program.py
+++ sorting_demo/src/sorting_demo/program.py
@@ -14,15 +14,27 @@ def main():
     # Load Gazebo Models via Spawning Services
     # Note that the models reference is the /world frame
     # and the IK operates with respect to the /base frame
-    model_list = gazebo_models.load_gazebo_models()
+    model_list, original_block_poses = gazebo_models.load_gazebo_models()
     # Remove models from the scene on shutdown
 
     rospy.on_shutdown(functools.partial(gazebo_models.delete_gazebo_models, model_list))
 
     task_planner = TaskPlanner()
-    task_planner.run()
-
     task_facade = task_planner.get_task_facade()
+
+    task_facade.start()
+
+    #rospy.sleep(40)
+    #task_facade.put_all_contents_on_table()
+
+    #task_facade.give_me_piece("GREEN")
+    #task_facade.pause()
+
+    #rospy.sleep(30)
+    #task_facade.resume()
+
+    #task_facade.stop("GREEN")
+
 
     task_planner.spin()
 
