57b6f0e91af2233044802af8de9775c443ec5422
Pablo Iñigo Blasco <pibgeus@gmail.com>
2018-08-31 07:38:21 +0200

    move block top view for each block

--- sorting_demo/src/sorting_demo/concepts/block.py
+++ sorting_demo/src/sorting_demo/concepts/block.py
@@ -12,6 +12,18 @@ class BlockState:
         search = BlockState.regex.search(id)
         self.num = int(search.group(1))
 
+        # the 2d blob point estimabion based on the head_image processing
+        self.hue_estimation = None
+
+        # the color estimation based on the head image processing
+        self.hue_estimation = None
+
+        # the 3d pose estimation from the head image processing
+        self.headview_pose_estimation =None
+
+    def __str__(self):
+        return "[Block estpos = %s]"%str(self.headview_pose_estimation)
+
     @staticmethod
     def is_block(id):
         num = BlockState.regex.search(id)
