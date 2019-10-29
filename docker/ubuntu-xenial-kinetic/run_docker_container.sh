e99ffa92df16d3cb75d2a466984700af9809712b
Pablo Iñigo Blasco <pibgeus@gmail.com>
2019-10-29 13:58:55 +0100

    sawyer runtime tests

--- docker/ubuntu-xenial-kinetic/run_docker_container.sh
+++ docker/ubuntu-xenial-kinetic/run_docker_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker run --gpus device=0 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env QT_X11_NO_MITSHM=1 --privileged -it ubuntu1604_sawyer /bin/bash
+sudo docker run --rm --gpus device=0 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env QT_X11_NO_MITSHM=1 --privileged -it ubuntu1604_sawyer /bin/bash
 RES=$?
 
 echo "RUNTIME TEST RESULT: $RES"
--- docker/ubuntu-xenial-kinetic/runtime_test.py
+++ docker/ubuntu-xenial-kinetic/runtime_test.py
@@ -5,29 +5,76 @@ import os
 import rospy
 from threading import Thread
 import time
+import signal
+import requests
 
 #---------------------------------------------
 def exit_properly_runtime_test():
     f = open("result.txt", "w")
     f.write("0")
     f.close()
+    rospy.signal_shutdown("SUCCESS TEST")
+    os.kill(os.getpid(), signal.SIGUSR1)
     sys.exit(0)
+
+class ExitCommand(Exception):
+    pass
+
+def signal_handler(signal, frame):
+    sys.exit(1)
 
 #timeout countdown
 def start_countdown(seconds):
-    def timeout_countdown(secibds):
+    def timeout_countdown(seconds):
         time.sleep(seconds)
         rospy.logfatal("COUNTDOWN ERROR RUNTIME TEST")
+        os.kill(os.getpid(), signal.SIGUSR1)
         sys.exit(1)
-
-    t= Thread(target = lambda: timeout_countdown(10))
+        
+    t= Thread(target = lambda: timeout_countdown(seconds))
     t.start()
 #---------------------------------------------
 # launch countdown
 
-start_countdown(4)
-time.sleep(3)
+signal.signal(signal.SIGUSR1, signal_handler)
 
+import gazebo_msgs.msg
+import intera_core_msgs.msg
+rospy.init_node("test_node")
+start_countdown(320)
+rospy.logwarn("[Test Node] waiting from gazebo message, first time can be slow (gazebo has to download models)...")
+rospy.wait_for_message("/gazebo/link_states", gazebo_msgs.msg.LinkStates,timeout=200)
 
-exit_properly_runtime_test()
+# wait until box is picked up
+def on_boxes_state_message(msg):
+    #rospy.loginfo("received from test node: " + str(msg))
+
+    str_msg = ""
+    exit_flag = False
+    for i, pose in enumerate(msg.pose):
+        name = msg.name[i]    
+        if "block" in name:
+            str_msg +=  str(name) + ".y: "+ str(pose.position.y) +"\n"
+            if pose.position.y >0.75:
+                exit_flag = True
+                rospy.logwarn("success, cube properly sent to tray. Ending!")
+                break
+
+    rospy.loginfo_throttle(1.0, "[Test Node] "+ str_msg)
+    
+    if exit_flag:
+        exit_properly_runtime_test()
+
+sub = rospy.Subscriber("/gazebo/link_states", gazebo_msgs.msg.LinkStates, on_boxes_state_message)
+
+rospy.logwarn("[Test Node] waiting sorting demo...")
+rospy.wait_for_message("/robot/limb/right/joint_command", intera_core_msgs.msg.JointCommand, timeout=60)
+
+rospy.logwarn("[Test Node] Calling start sorting...")
+rospy.sleep(10)
+res = requests.get('http://localhost:5000/start')
+rospy.logwarn(str(res))
+
+rospy.spin()
+
 
