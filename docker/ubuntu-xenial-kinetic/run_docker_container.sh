cdcd2776b525c6f67c54e33661f3ca5216e18128
Ubuntu <nvagent@nv6-nvidia-docker-build-agent-1.vehvaplzfk5unjbnbkump3y4rf.xx.internal.cloudapp.net>
2019-11-13 09:35:27 +0000

    runtime test

--- docker/ubuntu-xenial-kinetic/run_docker_container.sh
+++ docker/ubuntu-xenial-kinetic/run_docker_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo docker run --rm --gpus device=0 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env QT_X11_NO_MITSHM=1 --privileged -it ubuntu1604_sawyer
+sudo startxfce4&
+sleep 2;
+export DISPLAY=:0
+sudo docker run --rm --gpus device=0 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env QT_X11_NO_MITSHM=1 --privileged -t ubuntu1604_sawyer
 RES=$?
 
 echo "RUNTIME TEST RESULT: $RES"
